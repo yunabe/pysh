b8a3a93b644fcbedbfeb62cb74ac7ca9fef04697
Yu Watanabe <yunabe.public@gmail.com>
2012-08-30 13:00:35 +0900

    Fixed a bug.

--- pysh/shell/runner.py
+++ pysh/shell/runner.py
@@ -1,8 +1,6 @@
 import pysh.shell.builtin
 import pysh.shell.evaluator
 
-pysh.shell.builtin.register_builtin()
-
 def run(cmd_str, globals, locals, responses=None):
   rc = pysh.shell.evaluator.run(cmd_str, globals, locals)
   if not responses:
