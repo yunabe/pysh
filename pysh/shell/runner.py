a8944370757cd6523c667eeee19928a712cc2d32
Yu Watanabe <yunabe.public@gmail.com>
2012-09-10 19:19:43 +0900

    Accepts alias_map in pysh.shell.runner.run.

--- pysh/shell/runner.py
+++ pysh/shell/runner.py
@@ -1,8 +1,8 @@
 import pysh.shell.builtin
 import pysh.shell.evaluator
 
-def run(cmd_str, globals, locals, responses=None):
-  rc = pysh.shell.evaluator.run(cmd_str, globals, locals)
+def run(cmd_str, globals, locals, responses=None, alias_map=None):
+  rc = pysh.shell.evaluator.run(cmd_str, globals, locals, alias_map)
   if not responses:
     return None
   result = []
